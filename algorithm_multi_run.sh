7e8b7efb15ee3f8c4e32686bf82a55c005993f1d
Filip Ilievski <filip.dbrsk@gmail.com>
2015-11-16 16:57:57 +0100

    Langdetect library on git

--- algorithm_multi_run.sh
+++ algorithm_multi_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #./setup_stuff.sh
-./algorithm_run_curl.sh ad &
-./algorithm_run_curl.sh bc &
-./algorithm_run_curl.sh ef &
-./algorithm_run_curl.sh 01 &
-./algorithm_run_curl.sh 23 &
-./algorithm_run_curl.sh 45 &
-./algorithm_run_curl.sh 67 &
-./algorithm_run_curl.sh 89 &
+./algorithm_run_fs.sh ad &
+./algorithm_run_fs.sh bc &
+./algorithm_run_fs.sh ef &
+./algorithm_run_fs.sh 01 &
+./algorithm_run_fs.sh 23 &
+./algorithm_run_fs.sh 45 &
+./algorithm_run_fs.sh 67 &
+./algorithm_run_fs.sh 89 &
